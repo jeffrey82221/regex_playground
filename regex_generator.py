a5cca3584823fe64635cf87f79d52b487e56b523
jeffrey82221 <jeffrey82221@gmail.com>
2023-05-16 23:19:47 +0800

    feature: generate multiple examples with confidence of fullmatch

--- regex_generator.py
+++ regex_generator.py
@@ -6,7 +6,7 @@ regex for fullmatching is simpler than that for search or match
 
 TODO:
 - [X] use cuckoo filter to ignore repeat regex
-- [ ] generate multiple examples with length > 0
+- [X] generate multiple examples with length > 0
 - [ ] add selection weight to different kind of special character
 - [ ] speed up the generation using multi-processing
 """
@@ -20,7 +20,7 @@ from src.random_pattern import GroupGenerator
 
 
 class RegexGenerator:
-    def __init__(self, max_complexity=10, max_length=30):
+    def __init__(self, max_complexity=1000, max_length=50):
         self._max_complexity = max_complexity
         self._max_length = max_length
         self._pattern_getter = GroupGenerator(
@@ -52,13 +52,15 @@ class RegexGenerator:
                 'length': len(rp.regex)
             }),
             curried.filter(
-                lambda x: x['complexity'] >= 2 and x['complexity'] < self._max_complexity),
+                lambda x: x['complexity'] > 0 and x['complexity'] < self._max_complexity),
             curried.filter(lambda x: x['length'] >=
                            0 and x['length'] < self._max_length),
-            curried.filter(lambda x: self.valid(x['regex'])),
+            curried.filter(lambda x: self.fullmatch_valid(x['regex'])),
+            curried.map(self.add_examples),
+            curried.filter(lambda x: isinstance(x['examples'], list)),
+            curried.filter(lambda x: len(x['examples']) == x['complexity']),
+            curried.filter(self.all_full_match),
             self.non_repeat,
-            curried.map(self.add_example),
-            curried.filter(lambda x: len(x['example']) > 0)
         )
 
     def non_repeat(self, iterable):
@@ -67,11 +69,31 @@ class RegexGenerator:
                 yield x
                 self._bloom.add(x['regex'])
 
-    def valid(self, regex_str: str) -> bool:
+    def fullmatch_valid(self, regex_str: str) -> bool:
+        """
+        Assert fullmatching is possible
+        """
         example = exrex.getone(regex_str)
         return bool(re.compile(regex_str).fullmatch(example))
 
-    def add_example(self, result):
+    def all_full_match(self, result: dict) -> bool:
+        com = re.compile(result['regex'])
+        answers = []
+        for example in result['examples']:
+            try:
+                ans = bool(com.fullmatch(example))
+            except TypeError as e:
+                if e.args[0] == 'expected string or bytes-like object':
+                    return False
+                else:
+                    raise e
+            answers.append(ans)
+        return all(answers)
+
+    def add_example(self, result: dict) -> dict:
+        """
+        Add one single example
+        """
         result['example'] = exrex.getone(result['regex'])
         while not bool(re.compile(
                 result['regex']).fullmatch(result['example'])):
@@ -82,10 +104,25 @@ class RegexGenerator:
         """
         Enable generating of multiple examples
         """
-        pass
+        regex = result['regex']
+        try:
+            examples = []
+            for example in exrex.generate(regex):
+                examples.append(example)
+            result['examples'] = examples
+            return result
+        except TypeError as e:
+            if e.args[0] == 'can only concatenate list (not "str") to list':
+                result['examples'] = None
+                return result
+            elif e.args[0] == 'can only concatenate str (not "list") to str':
+                result['examples'] = None
+                return result
+            else:
+                raise e
 
 
 if __name__ == '__main__':
     gen = RegexGenerator().generate()
     for i, x in enumerate(gen):
-        print(i, x)
+        print(i, x['regex'], len(x['examples']))
